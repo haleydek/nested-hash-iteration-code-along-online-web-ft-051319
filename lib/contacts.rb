194fcdf77cd38769c5bf031fae16ff5b16d92ac3
Haley DeKeyser <hcd09@live.com>
2019-05-15 14:30:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         value.each do |flavor|
-          delete_if(flavor) == "strawberry"
+          delete(flavor) == "strawberry"
         end
       end
     end
